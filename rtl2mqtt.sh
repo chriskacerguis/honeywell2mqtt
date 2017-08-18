640c5cb321adecc347c9816ce78f8de805104b12
Chris Kacerguis <chriskacerguis@gmail.com>
2017-08-18 18:54:52 -0500

    removed -i option

--- rtl2mqtt.sh
+++ rtl2mqtt.sh
@@ -13,6 +13,6 @@ PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 do
   # Create file with touch /tmp/rtl_433.log if logging is needed
   [ -w /tmp/rtl_433.log ] && echo $line >> rtl_433.log
-  echo $line | /usr/bin/mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASS -i RTL_433 -r -l -t $MQTT_TOPIC
+  echo $line | /usr/bin/mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASS -r -l -t $MQTT_TOPIC
 done
 
