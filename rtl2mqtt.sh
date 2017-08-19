14d41116df4c08e8585730cd0aeb44d63dd109e0
Chris Kacerguis <chriskacerguis@gmail.com>
2017-08-18 21:11:37 -0500

    another try

--- rtl2mqtt.sh
+++ rtl2mqtt.sh
@@ -8,10 +8,16 @@
 export LANG=C
 PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 
+echo "Sending data to MQTT..."
+echo "Host: $MQTT_HOST"
+echo "User: $MQTT_USER"
+echo "Pass: $MQTT_PASS"
+echo "Topic: $MQTT_TOPIC"
+
 # Start the listener and enter an endless loop
 /usr/local/bin/rtl_433 -f 345000000 -F json -R 70 -U | while read line
 do
   # Create file with touch /tmp/rtl_433.log if logging is needed
   [ -w /tmp/rtl_433.log ] && echo $line >> rtl_433.log
-  echo $line | /usr/bin/mosquitto_pub -h localhost -u guest -P guest -r -l -t "homeassistant/sensor/honeywell"
+  echo $line | /usr/bin/mosquitto_pub -h localhost -u guest -P guest -i RTL_433 -r -l -t "homeassistant/sensor/honeywell"
 done
