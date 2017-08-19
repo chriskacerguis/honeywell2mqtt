533038d4f556d36d2c546cec8cba1410713bbdea
Chris Kacerguis <chriskacerguis@gmail.com>
2017-08-18 20:46:57 -0500

    another attempt to fix

--- rtl2mqtt.sh
+++ rtl2mqtt.sh
@@ -8,16 +8,10 @@
 export LANG=C
 PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 
-echo "Starting Honeywell Listener..."
-echo "Host: $MQTT_HOST"
-echo "User: $MQTT_USER"
-echo "Pass: $MQTT_PASS"
-echo "Topic: $MQTT_TOPIC"
-
 # Start the listener and enter an endless loop
 /usr/local/bin/rtl_433 -f 345000000 -F json -R 70 -U | while read line
 do
   # Create file with touch /tmp/rtl_433.log if logging is needed
   [ -w /tmp/rtl_433.log ] && echo $line >> rtl_433.log
-  echo $line | /usr/bin/mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASS -r -l -t $MQTT_TOPIC
+  echo $line | /usr/bin/mosquitto_pub -h localhost -u guest -P guest -r -l -t "homeassistant/sensor/honeywell"
 done
