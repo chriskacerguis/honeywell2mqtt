1290f0ba480c5dc6330e616a4961c22400fbf8e8
Chris Kacerguis <chriskacerguis@gmail.com>
2017-08-20 19:53:10 -0500

    removed U to see if I can get a local time

--- rtl2mqtt.sh
+++ rtl2mqtt.sh
@@ -11,7 +11,7 @@ export LANG=C
 PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 
 # Start the listener and enter an endless loop
-/usr/local/bin/rtl_433 -f 345000000 -F json -R 70 -U | while read line
+/usr/local/bin/rtl_433 -f 345000000 -F json -R 70 | while read line
 do
   # Create file with touch /tmp/rtl_433.log if logging is needed
   [ -w /tmp/rtl_433.log ] && echo $line >> rtl_433.log
