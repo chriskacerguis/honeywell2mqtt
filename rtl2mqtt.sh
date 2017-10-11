22498167e2db31a47e5cad1a6acee1adfd6e6551
james-fry <nospam_github@jamesfry.co.uk>
2017-10-11 14:09:24 +0100

    rtl433 test

--- rtl2mqtt.sh
+++ rtl2mqtt.sh
@@ -10,11 +10,14 @@ set -x
 export LANG=C
 PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 
+touch /tmp/rtl_433.log
+
 # Start the listener and enter an endless loop
 echo "Starting RTL..."
-/usr/local/bin/rtl_433 -f 345000000 -F json -R 70 | while read line
+#/usr/local/bin/rtl_433 -f 345000000 -F json -R 70 | while read line
+/usr/local/bin/rtl_433 -a | while read line
 do
   # Create file with touch /tmp/rtl_433.log if logging is needed
   [ -w /tmp/rtl_433.log ] && echo $line >> rtl_433.log
-  echo $line | /usr/bin/mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASS -i RTL_433 -r -l -t $MQTT_TOPIC
+  #echo $line | /usr/bin/mosquitto_pub -h $MQTT_HOST -u $MQTT_USER -P $MQTT_PASS -i RTL_433 -r -l -t $MQTT_TOPIC
 done
