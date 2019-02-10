8f9c18a2e4dbfa997f4afee9b9324e7253e550bc
Chris Kacerguis <chris@fuzzyblender.com>
2019-02-10 13:40:01 -0600

    fixes

--- send.js
+++ send.js
@@ -2,6 +2,7 @@
 
 const program = require('commander')
 const mqtt = require('mqtt')
+const fs = require("fs")
 
 program
   .version('0.2.0')
@@ -12,20 +13,18 @@ const client  = mqtt.connect('mqtt://' + process.env.MQTT_HOST)
 
 const discoveryPrefix = 'homeassistant'
 
-
 client.on('connect', function () {
   let packet = JSON.parse(program.packet)
 
   // Send the discovery message
   let configPayload = `{
-    "name": ${packet.id},
     "stat_t": "${discoveryPrefix}/binary_sensor/${packet.id}/state",
     "qos": 1,
     "pl_on": "open",
     "pl_off": "closed",
     "dev_cla": "opening"
   }`
-  client.publish(`${discoveryPrefix}/binary_sensor/${packet.id}/config`, JSON.parse(configPayload))
+  client.publish(`${discoveryPrefix}/binary_sensor/${packet.id}/config`, configPayload.replace(/\s/g,''))
   
 
 
